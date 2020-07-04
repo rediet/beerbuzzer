b669cbbbc1868d84a8009f759b0fa720f44f505f
Remo Diethelm <remo@remos-macbook-pro.home>
2020-07-04 13:47:00 +0200

    setup WiFi

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,15 @@
 #include <ESP8266SAM.h>
 #include <AudioOutputI2S.h>
 
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+
+// This file includes the declare statement for SECRET_SSID, SECRERT_PASS
+#include <secrets.h>
+
 const uint16_t PixelCount = 21;
+const char *ssid = SECRET_SSID;
+const char *password = SECRET_PASS;
 
 NeoPixelBus<NeoRgbFeature, NeoEsp8266Uart0800KbpsMethod> strip(PixelCount);
 OneButton button(D1, true);
@@ -47,12 +55,24 @@ void setup()
   strip.Begin();
   strip.Show();
 
+  // setup wifi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+  }
+
+  // setup button
   button.attachClick(click);
 
-  audio->begin();      
+  // setup audio
+  audio->begin();
+  audio->SetGain(0.5);    
 }
 
 void loop()
 {
+  // check for button press
   button.tick();
 }
